2ce03d0f30f57147e20653c8c96cf1a83a36bc37
amitraut <amitram902@gmail.com>
2018-06-13 00:31:22 +0530

    this is my python coding

---
+++ assignment1.py
@@ -0,0 +1,119 @@
+#1.To print anything on the screen
+print("queston no.1")
+x=5
+print(x)
+print(type(x))
+print("\n")
+
+
+
+x=1.5
+print(x)
+print(type(x))
+print("\n")
+
+
+
+x='a'
+print(x)
+print(type(x))
+print("\n")
+
+
+
+x='abcdefg'   
+print(x)
+print(type(x))
+print("\n")
+
+
+
+x=5
+x=x+10j
+print(x)
+print(type(x))
+print("\n")
+
+
+
+x="""This is 
+   python
+   Class """
+print(x)
+print(type(x))
+print("\n")
+
+#2.To join two string using '+' "Acad" + "view"
+print("queston no.2")
+x="Acad"
+y="view"
+z=x+y
+print(z)
+print("\n")
+
+x="Amit + "
+y="Raut"
+z=x+y
+print(z)
+print("\n")
+
+#3.Take input 3 variable and  print their variable on screen.
+print("queston no.3")
+x="Amit "
+y="raut"
+z=x+y
+print(z)
+print(type(z))
+print("\n")
+
+x="This is "
+y="python "
+z= "Class "
+u=x+y+z
+print(u)
+print(type(u))
+print("\n")
+
+
+#4.To print by taking input from user
+print("queston no.4")
+x=int(input("enter a number: "))
+y=int(input("enter another number: "))
+z=x+y
+print("Thesum is: ",z)
+print("\n")
+
+
+#5. to print the values using placeholder.
+print("queston no.5")
+x=10
+y=1.2
+z='a'
+print(x,y,z, sep = " __ ")
+
+print("\n")
+
+x=10
+y=1.2
+z='a'
+print(x,y,z, sep = " __ ",end="!!")
+
+print("\n")
+
+print ("hello world !!\n")
+
+print("\n")
+
+
+#6.lets do something intresting /to print/
+print("queston no.6")
+name="Tany stark"
+salary=100000
+print("%s,%d"%(name,salary))
+print("\n")
+
+print("queston no.7")
+p="Amit raut"
+course ="python"
+fees=5000
+print("%s,%s,%d"%(p,course,fees))
